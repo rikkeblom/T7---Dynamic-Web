f6b945b0d0b42b5910b88ad0a29cfc4f4b1ce5a2
Rikke Blom <rikk957v@stud.kea.dk>
2021-02-12 18:17:29 +0100

    edited-style

--- js/product.js
+++ js/product.js
@@ -17,7 +17,19 @@ function showProduct(product) {
   document.querySelector(
     "#product img"
   ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
-  document.querySelector(".price").textContent = product.price;
+
+  document.querySelector(".price").textContent = `DKK ${Math.round(
+    (product.price / 100) * (100 - product.discount)
+  )},-`;
+
+  document.querySelector("div.savings").textContent = `YOU SAVE DKK ${
+    product.price - Math.round((product.price / 100) * (100 - product.discount))
+  },-`;
+
+  if (product.discount) {
+    document.querySelector("div.savings").classList.remove("hidden");
+  }
+
   document.querySelector(".color").textContent = product.basecolour;
   document.querySelector(".description").innerHTML = product.description;
   document.querySelector(".type").textContent = product.category;
